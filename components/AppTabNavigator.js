30c63d51fec0fd2b50d7743e36f1d4250cacb29b
anjanamj <anjanamj@gmail.com>
2021-06-22 19:05:17 +0530

    Update AppTabNavigator.js

--- components/AppTabNavigator.js
+++ components/AppTabNavigator.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import Image from 'react-native';
-import HomeScreen from '../Screens/HomeScreen';
-import { AppStackNavigator } from './AppStackNavigator';
+import { Image } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import HomeScreen from '../Screens/HomeScreen';
+import ExchangeScreen from '../Screens/ExchangeScreen';
+
+
 
 export const AppTabNavigator = createBottomTabNavigator({
-    Exchange: ({
-        screen: AppStackNavigator,
-        navigationOptions: {
-            tabBarIcon: <Image source={require('../assets/exchange.png')} style={{ width: 20, height: 20 }} />,
-            tabBarlabel: "Exhange"         
-        },
-        Home: ({
-            screen: HomeScreen,
-            navigationOptions: {
-                tabBarIcon: <Image source={require("../assets/home.png")} />,
-                tabBarlabel: "Home",
-            }
-        })
-    })
-})
+    ExchangeScreen : {
+    screen: ExchangeScreen,
+    navigationOptions :{
+        tabBarIcon: <Image source={require('../assets/exchange.png')} style={{ width: 20, height: 20 }} />,
+        tabBarlabel: "Exhange" 
+    }
+  },
+  Home: {
+    screen: HomeScreen,
+    navigationOptions :{
+        tabBarIcon: <Image source={require("../assets/home.png")} />,
+        tabBarlabel: "Home",
+    }
+  }
+});
+
+
+
