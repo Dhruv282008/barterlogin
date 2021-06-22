d5ea34174362e487afce45409db3a9b509444c92
anjanamj <anjanamj@gmail.com>
2021-06-22 19:04:20 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { createAppContainer, createSwitchNavigator,} from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
 import Welcome from './Screens/welcome.js';
-import { AppTabNavigator } from './components/AppTabNavigator';
+import { AppTabNavigator } from './components/AppTabNavigator'
+
 export default function App() {
   return (
-    <Welcome/>
+    <AppContainer/>
   );
 }
 
+
 const switchNavigator = createSwitchNavigator({
   welcome: { screen: Welcome },
-  //Add AppDrawerNavigator Reminder
-  BottomTab: { screen: AppTabNavigator }
-});
+  BottomTab:{screen: AppTabNavigator}
+})
 
 const AppContainer =  createAppContainer(switchNavigator);
+
+
